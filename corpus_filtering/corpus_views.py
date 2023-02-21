cc35d8cb74036bb16f5efdee0c11c39a30cfb258
Abhinav Patil <abhinavp@uw.edu>
2023-02-21 00:41:35 -0800

    Style changes, including from running Black over corpus_views.py and  filters.py

--- corpus_filtering/corpus_views.py
+++ corpus_filtering/corpus_views.py
@@ -1,21 +1,26 @@
 from nltk.corpus.reader.util import PickleCorpusView
 import stanza
 
+
 class PickleStanzaDocCorpusView(PickleCorpusView):
-    """Wrapper around NLTK's `PickleCorpusView` to read in pickled `stanza.Document` objects from file but yield the
-    constituent `Sentence` objects rather than the `Document` objects themselves.
+    """Wrapper around NLTK's `PickleCorpusView` to read in pickled `stanza.Document`
+    objects from file but yield the constituent `Sentence` objects rather than the
+    `Document` objects themselves.
 
-    Also necessary because of a bug in NLTK (at least as of v.3.8.1) that greatly impedes the functionality of
-    `PickleCorpusView`. See NLTK issues #2331,#3124 on Github.
+    Also necessary because of a bug in NLTK (at least as of v.3.8.1) that greatly
+    impedes the functionality of `PickleCorpusView`. See NLTK issues #2331,#3124 on
+    Github.
 
-    For more detailed documentation of this class and the methods below, please refer to the NLTK docs.
+    For more detailed documentation of this class and the methods below, please refer to
+    the NLTK docs.
     """
+
     def __init__(self, fileid, doc_block_size=1):
         super().__init__(fileid)
-        self._encoding = None # This fixes the bug with NLTK's PickleCorpusView
+        self._encoding = None  # This fixes the bug with NLTK's PickleCorpusView
         self.BLOCK_SIZE = doc_block_size
 
     def read_block(self, stream):
         docs = super().read_block(stream)
-        sents = [s for doc in docs for s in doc.sentences] # flatten sentence lists of all docs
+        sents = [s for doc in docs for s in doc.sentences]  # flatten Sentence lists
         return sents
