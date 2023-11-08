cd41ac933555be27f501b7f1856a227a7da0b38c
jumelet <jumeletjaap@gmail.com>
2023-11-08 23:45:01 +0100

    Resolve PR comments

--- evaluate/ppl.py
+++ evaluate/ppl.py
@@ -1,31 +1,38 @@
-from typing import Dict, List, Optional, Union
+from typing import Union
 
 import torch
 import torch.nn as nn
 from minicons import scorer
-from torch import Tensor
-
-
-def return_all_probs(log_probs: Tensor) -> float:
-    """Return all token probs"""
-    return log_probs
 
 
 def evaluate_ppl(
     model_name: Union[nn.Module, str],
     corpus_file: str,
     device: str = "cuda",
-    verbose: bool = True,
-) -> Dict[str, float]:
+) -> float:
+    """Returns the perplexity of a single corpus file.
+
+    Parameters
+    ----------
+    model_name : str | nn.Module
+        Either a model name that refers to a HF model,
+        or an initialised torch model.
+    corpus_file : str
+        Path to the corpus file for which ppl will be computed.
+    device : str
+        Model device, defaults to cuda.
+
+    Returns
+    -------
+    perplexity : float
+    """
     model = scorer.IncrementalLMScorer(model_name, device)
 
     with open(corpus_file) as f:
-        corpus = f.read().strip().split('\n')
-    
-    all_log_probs = model.sequence_score(
-        corpus, reduction=return_all_probs
-    )
-    
+        corpus = f.read().strip().split("\n")
+
+    all_log_probs = model.sequence_score(corpus, reduction=lambda probs: probs)
+
     mean_log_prob = torch.concatenate(all_log_probs).mean()
     perplexity = torch.exp(-mean_log_prob)
 
